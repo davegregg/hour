2316710cc63fef01213ef291657f207605b35679
davegregg <davegregg@gmail.com>
2017-04-16 13:05:49 -0400

    Fix a potential nil value

--- lib/hour.rb
+++ lib/hour.rb
@@ -11,7 +11,7 @@ module Hour
       @to_a = [@hours = hours.to_i, @minutes]
       @to_s = "#{'%02d' % @hours}:#{'%02d' % @minutes}" # Formats single-digits such that '1' will be '01'
       @to_seconds = (@hours * 60 + @minutes) * 60
-      @to_days = @hours / 24
+      @to_days = @hours / 24 || 0
       @to_hours_less_days = @hours - 24 * @to_days
       @to_formatted_s = "#{@to_days} day#{'s' unless @to_days == 1}, " +
                         "#{@to_hours_less_days} hour#{'s' unless @to_hours_less_days == 1}, " +
@@ -21,7 +21,7 @@ module Hour
         1,                   #month
         1 + @to_days,        #day
         @to_hours_less_days, #hour
-        @minutes,             #minute
+        @minutes,            #minute
         0,                   #second
         0                    #UTC    #TODO: gracefully handle both the new Time.zone format and the deprecated UTC
       )
