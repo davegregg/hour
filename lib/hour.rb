2b31e952522dda415487f81bea126fde101cea06
davegregg <davegregg@gmail.com>
2017-04-18 16:58:57 -0400

    Moved pry for debugging divmod

--- lib/hour.rb
+++ lib/hour.rb
@@ -40,7 +40,9 @@ module Hour
     end
 
     def self.from_base10(num)
-      binding.pry
+      if !num
+        binding.pry
+      end
       hour, minute = num.divmod(1)
       self.new(hour, minute * 60)
     end
